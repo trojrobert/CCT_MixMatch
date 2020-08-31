328bee9bd0c17d8538b592980473b475ac136f3f
trojrobert <trojrobert@gmail.com>
2020-08-31 05:14:35 -0400

    add model functions

--- base/__init__.py
+++ base/__init__.py
@@ -1,2 +1,3 @@
 from .base_dataset import *
 from .base_dataloader import *
+from .base_model import * s
---
+++ base/base_model.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import logging
+import torch.nn as nn 
+
+
+class BaseModel(nn.Module):
+
+    def __init__(self):
+        super(BaseModel, self).__init__()
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+    def forward(self):
+        raise NotImplementedError
